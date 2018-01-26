b76d40150abd9946e84186fb9447bffd40c8d38f
Takehiro Miyao <takehiro-miyao@gmo.jp>
2018-01-26 09:32:53 +0000

    update for production

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express'),
+    app = express(),
+    debug = require('debug')('server');
+
+app.listen(3000);
+
+app.use('/medical-record', express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+    if (res.headersSent) return next();
+    debug('err', 'NOT FOUND');
+    res.status(404).send({ result: 'not found' });
+    next();
+});
+
+app.use(function(err, req, res, next) {
+    debug('err', err);
+    if (!res.headersSent) res.status(500).send({ result: 'failure' });
+});
